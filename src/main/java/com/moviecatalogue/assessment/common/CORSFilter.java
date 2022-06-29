f0b028e3aa0eb77efd356c5bea537479337405e5
jaredaguilerazamora <jaredaguilera@gmail.com>
2022-06-29 12:59:02 -0400

    cors fix

--- src/main/java/com/moviecatalogue/assessment/common/CORSFilter.java
+++ src/main/java/com/moviecatalogue/assessment/common/CORSFilter.java
@@ -1,13 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.moviecatalogue.assessment.common;
 
 import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-/*@Component
+/**
+ *
+ * @author sotobotero
+ */
+@Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
 
@@ -45,4 +62,4 @@ public class CORSFilter implements Filter {
     @Override
     public void destroy() {
     }
-}*/
+}
